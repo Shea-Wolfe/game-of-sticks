6428cfb0d65f0ec510ff935db3107b08edd8a053
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-08 19:54:33 -0400

    Changing functions to allow selection of number of players.  defined a loop for ai v ai

--- ai_sticks.py
+++ ai_sticks.py
@@ -2,8 +2,11 @@ import random
 def game_setup(players):
     '''Takes one arguement, the players (one or two).
     Gets the player names and how many sticks the game will be played with'''
-
-    if players == 1:
+    if players == 0:
+        sticks = 100
+        player1 = 'Skynet'
+        player2 = 'Hal'
+    elif players == 1:
         sticks = int(input('Welcome to the game how many sticks are on the table?\nEnter a number between 10-100? > '))
         player1 = input('Great! player 1 please enter your name > ')
         player2 = 'Skynet'
@@ -26,11 +29,13 @@ def end_game(current_player, ai_dict):
     '''Takes the losing player as an arguements
     checks to see if the player wants to play again, if yes it runs the main loop
     if no it breaks the main loop which exits the program.'''
-    print(ai_dict)
     credit = input('You lose {}!  Would you like to play again? enter [y]es or [n]o >'.format(current_player)).lower()
     if credit == 'y' or credit == 'yes':
         return main(ai_dict)
     return True
+
+def end_game_ai(ai_dict):
+    return main(ai_dict)
 
 def ai_dict_changes(ai_dict, ai_choices, ai_win):
     '''This function takes the set dictionary, the choices it's made in the game and if it won.
@@ -47,6 +52,29 @@ def ai_dict_changes(ai_dict, ai_choices, ai_win):
                 ai_dict[key].remove(ai_choices[key])
     return ai_dict
 
+def dual_ai_dict_changes(ai_dict, ai_a_choices, ai_b_choices, ai_win):
+    '''This function takes the set dictionary, the both ai\'s have made and which one won
+    it returns a list that adds the winning ai\'s moves and subtracts the losing ai\'s moves
+    down to having one of each option.'''
+    if ai_win:
+        for key in ai_b_choices:
+            ai_dict[key].append(ai_b_choices[key])
+        for key in ai_a_choices:
+            if ai_dict[key].count(ai_a_choices[key]) == 1:
+                pass
+            else:
+                ai_dict[key].remove(ai_a_choices[key])
+
+    else:
+        for key in ai_a_choices:
+            ai_dict[key].append(ai_a_choices[key])
+        for key in ai_b_choices:
+            if ai_dict[key].count(ai_b_choices[key]) == 1:
+                pass
+            else:
+                ai_dict[key].remove(ai_b_choices[key])
+    return ai_dict
+
 def ai_turn(ai_dict, sticks, ai_choices):
     '''Takes two arguements, the AI dictionary and the number of sticks in the pile
     returns a random selection from remaining values at that key in the ai dict'''
@@ -60,13 +88,13 @@ def get_ai_dict():
     return {key: [1,2,3] for key in range(1,101)}
 
 
-def main(ai_dict):
+def game_loop(ai_dict):
     '''inform player turn and ask for # sticks to remove
     check to see if player removed the last stick
     if True, inform player he loses
     else continue'''
     ai_choices = {}
-    sticks,player1,player2 = game_setup(1)
+    sticks,player1,player2 = game_setup()
     while sticks > 0:
         sticks_out = int(input('Hi {}!  There are {} sticks remaining \nPlease select a number of sticks, 1-3 > '.format(player1,sticks)))
         while sticks_out not in range(1,4):
@@ -84,6 +112,27 @@ def main(ai_dict):
             if end_game(player2, ai_dict):
                 break
 
+def ai_game_loop(ai_dict, no_player=True):
+    '''Take's an AI dictionary and runs two ai's against themselves.
+    the winners moves are appended to the master dictionary and the losers
+    removed.  The function returns to the main function when done.'''
+    ai_a_choices = {}
+    ai_b_choices = {}
+    if player:
+        game_setup(0)
+    while sticks > 0:
+        sticks_out, ai_a_choices = ai_turn(ai_dict, sticks, ai_a_choices)
+        sticks = player_move(sticks, sticks_out)
+        if sticks < 1:
+            ai_dict = dual_ai_dict_changes(ai_dict, ai_a_choices, ai_b_choices, True)
+            if end_game(ai_dict):
+                break
+        sticks_out, ai_b_choices = ai_turn(ai_dict, sticks, ai_b_choices)
+        sticks = player_move(sticks, sticks_out)
+        if sticks <1:
+            ai_dict = dual_ai_dict_changes(ai_dict, ai_a_choices, ai_b_choices, False)
+            if end_game(ai_dict):
+                break
 
 
 
