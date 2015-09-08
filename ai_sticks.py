81c6e67df32b0dc030a2df1e90b8d6f656f47a12
William Butts <Shea_Wolfe@Williams-MacBook-Pro.local>
2015-09-08 17:38:21 -0400

    Added functions to keep track of ai guesses and change future guesses based on game results.  Untested

--- ai_sticks.py
+++ ai_sticks.py
@@ -22,20 +22,37 @@ def player_move(sticks, player_move):
     sticks -= player_move
     return sticks
 
-def end_game(current_player):
+def end_game(current_player, ai_dict):
     '''Takes the losing player as an arguements
     checks to see if the player wants to play again, if yes it runs the main loop
     if no it breaks the main loop which exits the program.'''
+    print(ai_dict)
     credit = input('You lose {}!  Would you like to play again? enter [y]es or [n]o >'.format(current_player)).lower()
     if credit == 'y' or credit == 'yes':
-        return main()
+        return main(ai_dict)
     return True
 
-def ai_turn(ai_dict, sticks):
+def ai_dict_changes(ai_dict, ai_choices, ai_win):
+    '''This function takes the set dictionary, the choices it's made in the game and if it won.
+    It returns a dictionary that has been changed based on the guesses and if it won
+    or didn't win'''
+    if ai_win:
+        for key in ai_choices:
+            ai_dict[key].append(ai_choices[key])
+    else:
+        for key in ai_choices:
+            if len(ai_dict[key]) < 2:
+                pass
+            else:
+                ai_dict[key].remove(ai_choices[key])
+    return ai_dict
+
+def ai_turn(ai_dict, sticks, ai_choices):
     '''Takes two arguements, the AI dictionary and the number of sticks in the pile
     returns a random selection from remaining values at that key in the ai dict'''
     current_guess = random.choice(ai_dict[sticks])
-    return current_guess
+    ai_choices[sticks] = current_guess
+    return (current_guess, ai_choices)
 
 def get_ai_dict():
     '''Takes no input.  Returns a dictionary with keys from 1-100.
@@ -43,11 +60,12 @@ def get_ai_dict():
     return {key: [1,2,3] for key in range(1,101)}
 
 
-def main():
+def main(ai_dict):
     '''inform player turn and ask for # sticks to remove
     check to see if player removed the last stick
     if True, inform player he loses
     else continue'''
+    ai_choices = {}
     sticks,player1,player2 = game_setup(1)
     while sticks > 0:
         sticks_out = int(input('Hi {}!  There are {} sticks remaining \nPlease select a number of sticks, 1-3 > '.format(player1,sticks)))
@@ -56,12 +74,14 @@ def main():
             sticks_out = int(input('As a reminder {} there are {} sticks remaing. \nPlease select a number of sticks, 1-3 > '.format(player1, sticks)))
         sticks = player_move(sticks, sticks_out)
         if sticks < 1:
-            if end_game(player1):
+            ai_dict = ai_dict_changes(ai_dict, ai_choices, True)
+            if end_game(player1, ai_dict):
                 break
-        sticks_out = ai_turn(ai_dict, sticks)
+        sticks_out, ai_choices = ai_turn(ai_choices, sticks)
         sticks = player_move(sticks, sticks_out)
         if sticks <1:
-            if end_game(player2):
+            ai_dict = ai_dict_changes(ai_dict, ai_choices, False)
+            if end_game(player2, ai_dict):
                 break
 
 
@@ -69,4 +89,4 @@ def main():
 
 if __name__ == '__main__':
     ai_dict = get_ai_dict()
-    main()
+    main(ai_dict)
