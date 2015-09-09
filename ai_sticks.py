93180c0eae45c3d834ddd8fb92e28c505d1a25b7
William Butts <wbutts1023@gmail.com>
2015-09-08 21:32:48 -0400

    Bug fixes.  Fixed non-int stick count and ending game

--- ai_sticks.py
+++ ai_sticks.py
@@ -1,16 +1,25 @@
 import random
+import sys
 def game_setup(players):
     '''Takes one arguement, the players (one or two).
     Gets the player names and how many sticks the game will be played with'''
     if players == 1:
-        sticks = int(input('Welcome to the game how many sticks are on the table?\nEnter a number between 10-100? > '))
+        try:
+            sticks = int(input('Welcome to the game how many sticks are on the table?\nEnter a number between 10-100? > '))
+        except:
+            print('That\'s not a number!  try again!')
+            return game_setup(1)
         if sticks not in range(10, 101):
             print('that\'s not between 10 and 100!')
             return game_setup(1)
         player1 = input('Great! player 1 please enter your name > ')
         player2 = 'Skynet'
     else:
-        sticks = int(input('Welcome to the game how many sticks are on the table?\nEnter a number between 10-100? > '))
+        try:
+            sticks = int(input('Welcome to the game how many sticks are on the table?\nEnter a number between 10-100? > '))
+        except:
+            print('That\'s not a number!  try again!')
+            return game_setup(2)
         if sticks not in range(10, 101):
             print('That\'s not between 10 and 100!')
             return game_setup(2)
@@ -31,10 +40,12 @@ def end_game(current_player, ai_dict):
     '''Takes the losing player as an arguements
     checks to see if the player wants to play again, if yes it runs the main loop
     if no it breaks the main loop which exits the program.'''
+    print(ai_dict)
     credit = input('You lose {}!  Would you like to play again? enter [y]es or [n]o >'.format(current_player)).lower()
     if credit == 'y' or credit == 'yes':
-        return main(ai_dict)
-    return True
+        return player_split(ai_dict)
+    else:
+        return True
 
 def end_game_ai(ai_dict, difficulty):
     return main(ai_dict, difficulty)
@@ -98,11 +109,30 @@ def players_split(ai_dict):
     players = int(input('Please enter number of players [1], or [2] > '))
     while  players > 2 or players < 1:
         players = int(input('I\'m sorry I didn\'t get that, was that [1] or [2] players? > '))
-    sticks,player1,player2 = game_setup()
+    sticks, player1, player2 = game_setup(players)
     if players == 2:
-        game_loop(sticks, player1, player2, ai_dict)
+        p_v_p_game_loop(sticks, player1, player2, ai_dict)
     else:
          p_v_ai_game_loop(sticks, player1, player2, ai_dict)
+
+def p_v_p_game_loop(sticks, player1, player2, ai_dict):
+    while sticks > 0:
+        sticks_out = int(input('Hi {}!  There are {} sticks remaining \nPlease select a number of sticks, 1-3 > '.format(player1,sticks)))
+        while sticks_out not in range(1,4):
+            print('That\'s not a valid move!')
+            sticks_out = int(input('As a reminder {} there are {} sticks remaing. \nPlease select a number of sticks, 1-3 > '.format(player1, sticks)))
+        sticks = player_move(sticks, sticks_out)
+        if sticks < 1:
+            if end_game(player1, ai_dict):
+                sys.exit()
+        sticks_out = int(input('Hi {}!  There are {} sticks remaining \nPlease select a number of sticks, 1-3 > '.format(player2, sticks)))
+        while sticks_out not in range(1,4):
+            print('That\'s not a valid move!')
+            sticks_out = int(input('As a reminder {} there are {} sticks remaing. \nPlease select a number of sticks, 1-3 > '.format(player2, sticks)))
+        sticks = player_move(sticks, sticks_out)
+        if sticks <1:
+            if end_game(player2, ai_dict):
+                sys.exit()
 
 def p_v_ai_game_loop(sticks, player1, player2, ai_dict):
     ai_choices = {}
@@ -115,22 +145,21 @@ def p_v_ai_game_loop(sticks, player1, player2, ai_dict):
         if sticks < 1:
             ai_dict = ai_dict_changes(ai_dict, ai_choices, True)
             if end_game(player1, ai_dict):
-                break
+                sys.exit()
         sticks_out, ai_choices = ai_turn(ai_dict, sticks, ai_choices)
         sticks = player_move(sticks, sticks_out)
         if sticks <1:
             ai_dict = ai_dict_changes(ai_dict, ai_choices, False)
             if end_game(player2, ai_dict):
-                break
+                sys.exit()
 
-def ai_game_loop(ai_dict, difficulty, no_player=True):
+def ai_game_loop(ai_dict, difficulty):
     '''Take's an AI dictionary and runs two ai's against themselves.
     the winners moves are appended to the master dictionary and the losers
     removed.  The function returns to the main function when done.'''
     ai_a_choices = {}
     ai_b_choices = {}
-    if player:
-        game_setup(0)
+    sticks = 100
     while sticks > 0:
         sticks_out, ai_a_choices = ai_turn(ai_dict, sticks, ai_a_choices)
         sticks = player_move(sticks, sticks_out)
@@ -153,15 +182,15 @@ def difficulty():
     if difficulty not in 'emh':
         return difficulty()
     elif difficulty == 'e':
-        return 100
+        return 250
     elif difficulty == 'm':
-        return 50
+        return 200
     else:
         return 0
 def main(ai_dict, difficulty):
-    while difficulty < 100:
+    while difficulty < 250:
         ai_game_loop(ai_dict, difficulty)
-    game_loop(ai_dict)
+    players_split(ai_dict)
 
 
 if __name__ == '__main__':
